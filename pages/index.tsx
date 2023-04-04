0c76290fa4f02d7e762feb4d15148898de2ef819
Matt <foxmattkraft@outlook.com>
2023-04-04 15:54:09 -0400

    added contactt us form

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ import useMeasure from "react-use-measure";
 import { useTrail, animated } from "@react-spring/web";
 import Footer from "@/components/sections/Footer";
 import Meta from "@/components/Meta";
+import ContactUs from "@/components/sections/ContactUs";
 
 export default function Home() {
   useEffect(() => {
@@ -32,6 +33,7 @@ export default function Home() {
       <ServiceSection />
       <MenuSection />
       <FoodSection />
+      <ContactUs/>
       <Footer />
     </>
   );
